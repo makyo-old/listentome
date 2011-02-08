d71b93a606034ef88cb8d732e445367f5ca2aa68
Matthew Scott <mjs@mjs-svc.com>
2011-02-08 15:17:30 -0700

    Tiny work.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,13 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('listentome.catalog.views',
-    (r'record/(?P<record_id>\d+)/$', 'show_record'),
+    (r'^record/(?P<record_id>\d+)/$', 'show_record'),
+    (r'^performer/(?P<performer_id>\d+)/$', 'show_performer'),
+    (r'^piece/(?P<piece_id>\d+)/$', 'show_piece'),
+    (r'^composer/(?P<composer_id>\d+)/$', 'show_composer'),
+    
+    (r'^record/add/$', 'create_record'),
+
+    (r'^ajax/performer/add/$', 'ajax_create_performer'),
+    (r'^ajax/piece/add/$'
 )
