6678a5c18dd6282d71ef12e89568d04d018b82ef
Madison Scott-Clary <madison@archive.org>
2019-02-01 14:34:38 -0800

    old work

--- circulation/views.py
+++ circulation/views.py
@@ -9,23 +9,34 @@ from listentome.catalog.models import *
 @permission_required("record.can_checkout")
 def checkout(request, record_id):
     record = get_object_or_404(Record, pk = record_id)
-if request.method == "GET":
-        return render_to_response("circulation/checkout.html",
-                context_instance = RequestContext(request,
-                    {'record': record}))
-    else:
-        form = CheckoutForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(record.get_absolute_url())
-        else:
+    if record.status == 'a' or record.status = 'r':
+        if request.method == "GET":
             return render_to_response("circulation/checkout.html",
                     context_instance = RequestContext(request,
-                        {'record': record, 'form': form}))
+                        {'record': record}))
+        else:
+            form = CheckoutForm(request.POST)
+            if form.is_valid():
+                can_check_out = False
+                if record.status == 'a':
+                    can_check_out = True
+                elif record.status == 'r':
+                    for reservation in record.reserverecord_set.all():
+                        if reservation.reserved_for == form.user:
+                            can_check_out = True
+                            break
+                if can_check_out:
+                    form.save()
+                    return HttpResponseRedirect(record.get_absolute_url())
+            else:
+                return render_to_response("circulation/checkout.html",
+                        context_instance = RequestContext(request,
+                            {'record': record, 'form': form}))
+    # Return message saying unable to check out
 
 @permission_required("record.can_checkin")
 def checkin(request, record_id):
-    pass
+    record = get_object_or_404(Record, pk = record_id)
 
 @login_required
 def reserve(request, record_id):
